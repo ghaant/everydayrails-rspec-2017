859e9ef5bd1c30454a809a857e7c3d488ca579e4
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-04-28 17:53:39 +0200

    Authenticated controler specs.

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Projects
                        view_specs: false,
                        helper_specs: false,
                        routing_specs: false,
-                       request_specs: false
+                       controller_specs: true
     end
   end
 end
