25594048c37007d8ed7152d371b30abde50dd0bc
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-05-01 12:10:31 +0200

    Replace "before" blocks with "let".

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,19 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  before do
-    @user = FactoryBot.create(:user)
-
-    @project = @user.projects.create(
-      name: 'Test Project'
-    )
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
 
   it 'is valid with a user, project, and message' do
     note = Note.new(
       message: 'This is a sample note.',
-      user: @user,
-      project: @project
+      user: user,
+      project: project
     )
 
     expect(note).to be_valid
@@ -27,25 +22,37 @@ RSpec.describe Note, type: :model do
   end
 
   describe 'search message for a term' do
-    before do
-      @note1 = @project.notes.create(
-        message: 'This is the first note.',
-        user: @user
+    let!(:note1) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'This is the first note.'
       )
-      @note2 = @project.notes.create(
-        message: 'This is the second note.',
-        user: @user
+    }
+
+    let!(:note2) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'This is the second note.'
       )
-      @note3 = @project.notes.create(
-        message: 'First, preheat the oven',
-        user: @user
+    }
+
+    let!(:note3) {
+      FactoryBot.create(
+        :note,
+        project: project,
+        user: user,
+        message: 'First, preheat the oven.'
       )
-    end
+    }
 
     context 'when a match is found' do
       it 'returns notes that match the search term' do
-        expect(Note.search('first')).to include(@note1, @note3)
-        expect(Note.search('first')).not_to include(@note2)
+        expect(Note.search('first')).to include(note1, note3)
+        expect(Note.search('first')).not_to include(note2)
       end
     end
 
